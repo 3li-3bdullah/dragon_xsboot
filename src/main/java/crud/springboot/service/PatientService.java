ece080b5019420ec1cf5722fcd1b5f9443903cc6
3li-3bdullah <PROGRMMER7@gmail.com>
2025-09-01 02:42:54 +0300

    - Added new service

--- src/main/java/crud/springboot/service/PatientService.java
+++ src/main/java/crud/springboot/service/PatientService.java
@@ -1,24 +1,35 @@
 package crud.springboot.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import crud.springboot.model.Patient;
 import crud.springboot.model.PatientModel;
 
 @Service
 public class PatientService {
 
     // public String printPatientName(String name) {
-    //     return name.toUpperCase();
+    // return name.toUpperCase();
     // }
 
     @Autowired
     private PatientModel patientModel;
 
-    public String printPatientName(String patientName) {
-        String name = patientModel.getPatients().stream().filter(i -> i == patientName).findAny().orElse("Not Found"); 
-        return name;
+    public Patient printPatientName(String patientName, Integer age) {
+        Patient patient = patientModel.getPatients().stream().filter(i -> {
+            return i.getName().equals(patientName) || i.getAge().equals(age); }).findAny().orElse(new Patient());
+        return patient;
+    }
+
+    public List<Patient> getPatients() {
+        // List<Patient> patients = new ArrayList<>();
+        // patientModel.getPatients().forEach(name -> patients.add(new Patient(name,
+        // 10)));
+
+        return patientModel.getPatients();
     }
 }
