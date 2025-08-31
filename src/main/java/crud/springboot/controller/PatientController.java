6f85883aff831ab62489c35e480d2af9bec1903c
3li-3bdullah <PROGRMMER7@gmail.com>
2025-09-01 02:41:56 +0300

    - added new methods

--- src/main/java/crud/springboot/controller/PatientController.java
+++ src/main/java/crud/springboot/controller/PatientController.java
@@ -1,10 +1,15 @@
 package crud.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import crud.springboot.model.Patient;
 import crud.springboot.service.PatientService;
 
 @RestController
@@ -15,7 +20,18 @@ public class PatientController {
     private PatientService patientService;
     
     @GetMapping("/print-name")
-    public String printPatientName() {
-        return patientService.printPatientName("John Doe");
+    public ResponseEntity printPatientName(@RequestParam String name, @RequestParam("a") Integer age) {
+        return ResponseEntity.ok(patientService.printPatientName(name,age));
+    }
+
+
+     @GetMapping("/patients")
+     public ResponseEntity getPatients() {
+         return ResponseEntity.ok(patientService.getPatients());
+     }
+    
+     @PostMapping("/patients/save")
+    public Patient savePatients(@RequestBody Patient patient) {
+        return patient;
     }
 }
